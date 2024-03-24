eb595fc0807c9c446075fd38889aad75b3a4f76c
lisiCAO <caolisi@hotmail.com>
2024-03-24 12:18:38 -0400

    add identity scaffolded items

--- BookWeb/Program.cs
+++ BookWeb/Program.cs
@@ -1,4 +1,4 @@
-﻿using Book.DataAccess.Data;
+using Book.DataAccess.Data;
 using Book.DataAccess.Repository;
 using Book.DataAccess.Repository.IRepository;
 using Book.Utility;
@@ -12,6 +12,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
